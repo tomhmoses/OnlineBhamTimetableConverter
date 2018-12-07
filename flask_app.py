bd45f5787f2b315f2b0cc24f33717a9e0466958b
Tom Moses <tom@tmoses.co.uk>
2018-12-07 16:46:14 +0000

    added a few more pages

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import BhamCalConverter
 
 app = Flask(__name__)
@@ -6,8 +6,8 @@ app.config["DEBUG"] = True
 
 #comments = []
 
-@app.route("/", methods=["GET", "POST"])
-def login():
+@app.route("/timetable/", methods=["GET", "POST"])
+def main():
     if request.method == "GET":
         return render_template("main_page.html", error=False, message = "")
 
@@ -19,6 +19,22 @@ def login():
     #message = BhamCalConverter.run(email, username, password)
     message = BhamCalConverter.runFromFlask(email, username, password)
     if message == "done":
-        return message
+        return redirect(url_for('done'))
 
     return render_template("main_page.html", error=True, message = message)
+
+@app.route("/")
+def timetable():
+    return redirect(url_for('main'))
+
+@app.route("/done/")
+def done():
+    return render_template("done_page.html")
+
+@app.route("/test/email/")
+def testEmail():
+    return render_template("test_email.html")
+
+@app.route("/share/")
+def share():
+    return render_template("share_page.html")
