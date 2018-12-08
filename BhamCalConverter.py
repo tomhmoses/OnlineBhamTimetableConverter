fb3433c56e6c1818abe9aa126817def06f9cbc89
Tom Moses <tom@tmoses.co.uk>
2018-12-08 21:01:08 +0000

    added statistics tracking of site visits

--- BhamCalConverter.py
+++ BhamCalConverter.py
@@ -3,6 +3,19 @@ import BhamTTConverter
 import BhamGoogleCalendarMaker
 import getpass
 import BhamCalEmailSender
+import pickle
+
+visitsFilePath = "visits.pickle"
+usernameLogFilePath = "usernameLog.txt"
+inUseFilePath = "inUse.pickle"
+
+def resetInUse():
+    inUse = False
+    savePickle(inUseFilePath, inUse)
+
+def setInUse():
+    inUse = True
+    savePickle(inUseFilePath, inUse)
 
 def main():
     email, username, password = getUserInput()
@@ -14,14 +27,21 @@ def runFromFlask(email, username, password):
     username = checkUsername(username)
     if username == "Invalid username":
         return username
-    if email == "":
-        print("uni email generated")
-        email = username + "@student.bham.ac.uk"
-    try:
-        message = run(email, username, password)
-    except Exception as e:
-        message = str(e)
-        message += "\nUsing:\n" + email + "\n" + username + "\n" + str(len(password)) + ". Please try again in a minute..."
+    if loadPickle(inUseFilePath):
+        print("was in use :(")
+        message = "Somebody else is using the service right now. Due to limitation with having a free google API account I can't handle more than one request at once. Please try again in 2 minutes..."
+    else:
+        setInUse()
+        if email == "":
+            print("uni email generated")
+            email = username + "@student.bham.ac.uk"
+        try:
+            message = run(email, username, password)
+        except Exception as e:
+            message = str(e)
+            message += "\nUsing:\n" + email + "\n" + username + "\n" + str(len(password)) + ". Please try again in 2 minutes..."
+        finally:
+            resetInUse()
     return message
 
 def checkUsername(username):
@@ -40,12 +60,54 @@ def run(email, username, password):
         print("error occurred in getting frame")
         errorMessage = frameSource
         return errorMessage
+    print("got frame source")
     saveToFile(frameSource, "frameSource.html")
     csv = BhamTTConverter.main(frameSource)
+    print("generated csv")
     saveToFile(csv, "Timetable.csv")
     linkToCal = BhamGoogleCalendarMaker.main(username, email, csv)
     BhamCalEmailSender.sendMail(email, linkToCal)
     return "done"
+
+def getStats():
+    try:
+        visits = visits = loadPickle(visitsFilePath)
+    except:
+        visits = 0
+    try:
+        file = open(usernameLogFilePath, "r")
+        usernames = file.readlines()
+        file.close()
+        users = len(usernames)
+    except:
+        visits = 0
+    stats = {"visits":visits, "users":users}
+    return stats
+
+def trackVisit():
+    attempts = 3
+    worked = False
+    while attempts > 0 and worked == False:
+        try:
+            trackVisitWithPickle()
+            worked = True
+        except:
+            attempts -= 1
+    if not worked:
+        print("failed to track site visit")
+
+def trackVisitWithPickle():
+    visits = loadPickle(visitsFilePath)
+    visits += 1
+    savePickle(visitsFilePath, visits)
+
+def savePickle(file_name, obj):
+    with open(file_name, 'wb') as fobj:
+        pickle.dump(obj, fobj)
+
+def loadPickle(file_name):
+    with open(file_name, 'rb') as fobj:
+        return pickle.load(fobj)
 
 
 def saveToFile(text, filePath = "output.txt"):
