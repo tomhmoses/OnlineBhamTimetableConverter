b5b07012ff64ddba36c66e435581d8b6f80da174
Tom Moses <tom@tmoses.co.uk>
2019-04-29 18:26:29 +0100

    changed logger name

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import logging
 
 def initilise_logging():
     logging.basicConfig(level=logging.INFO)
-    logger = logging.getLogger('food-flex')
+    logger = logging.getLogger('timetable-converter')
     logger.setLevel(logging.INFO)
-    handler = logging.FileHandler(filename='data/foodflex.log', encoding='utf-8', mode='w')
+    handler = logging.FileHandler(filename='data/ttc.log', encoding='utf-8', mode='w')
     handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
     logger.addHandler(handler)
     return logger
