9174e538e11239859f09c67f9804d2ad8d306a80
Tom Moses <tom@tmoses.co.uk>
2019-04-30 09:39:11 +0100

    can deal with short titles and custom titles
    fixed some indentation
    and fixed how datetime string is creates

--- BhamGoogleCalendarMaker.py
+++ BhamGoogleCalendarMaker.py
@@ -19,25 +19,24 @@ filePaths = [{"token":"/home/tomhmoses/mysite_ttc/token.json", "creds":"/home/to
             {"token":"/home/tomhmoses/mysite_ttc/token2.json", "creds":"/home/tomhmoses/mysite_ttc/credentials2.json"},
             {"token":"/home/tomhmoses/mysite_ttc/token3.json", "creds":"/home/tomhmoses/mysite_ttc/credentials3.json"}]
 
-def main(username, email, csv):
+def main(username, email, csv, shortenTitle, customTitle):
     for account_no in range(len(filePaths)):
         try:
-            return create_calendar(username, email, csv, account_no)
+            return create_calendar(username, email, csv, shortenTitle, customTitle, account_no)
         except:
             logger.warn("failed with account_no: " + str(account_no))
 
 
-def create_calendar(username, email, csv, account_no):
+def create_calendar(username, email, csv, shortenTitle, customTitle, account_no):
     store = file.Storage(filePaths[account_no]["token"])
     creds = store.get()
     if not creds or creds.invalid:
         flow = client.flow_from_clientsecrets(filePaths[account_no]["creds"], SCOPES)
         creds = tools.run_flow(flow, store, args)
     service = build('calendar', 'v3', http=creds.authorize(Http()))
-
-
-
     summary = "UoB Timetable: " + username
+    if customTitle != "":
+        summary = customTitle
     timeZone = "Europe/London"
     calendar = {
     'summary': summary,
@@ -52,20 +51,31 @@ def create_calendar(username, email, csv, account_no):
     for csvEvent in csv_events:
         details = csvEvent.split(",")
         if to_date_time(details[0], details[3]) == "":
-            logger.debug("error occurred setting datetime so skipped.")
+            logger.warn("error occurred setting datetime so skipped.")
         else:
+            if shortenTitle:
+                shortTitle = details[2]
+                for each in ["LM","LI","LC","LH","LANS"]:
+                    shortTitle = shortTitle.replace(each+"/", "")
+                    shortTitle = shortTitle.replace(each+" ", "")
+                s = shortTitle
+                shortTitle = s[:s.find("(")] + " · " + s[s.find(")/") + len(")/"):]
+                while "  " in shortTitle:
+                    shortTitle = shortTitle.replace("  ", " ")
+                logger.debug("made short title: " + shortTitle)
+                details[2] = shortTitle
             eventDetails = {
-              'summary': details[2],
-              'location': details[5],
-              'description': details[6],
-              'start': {
-                'dateTime': to_date_time(details[0], details[3]),
-                'timeZone': timeZone,
-              },
-              'end': {
-                'dateTime': to_date_time(details[1], details[4]),
-                'timeZone': timeZone,
-              },
+                'summary': details[2],
+                'location': details[5],
+                'description': details[6],
+                'start': {
+                    'dateTime': to_date_time(details[0], details[3]),
+                    'timeZone': timeZone,
+                },
+                'end': {
+                    'dateTime': to_date_time(details[1], details[4]),
+                    'timeZone': timeZone,
+                },
             }
             try:
                 event = service.events().insert(calendarId=calID, body=eventDetails).execute()
@@ -114,19 +124,22 @@ def to_date_time(date_string, time_string):
             date_arr[count] = "0" + date_arr[count]
     time_arr = time_string.split(":")
     if (time.tzname[0] == 'STD'):
+        logger.degub("was in DST so time moved back an hour to match UTC:")
+        logger.debug(date_arr)
+        logger.debug(time_arr)
         time_arr[0] = str(int(time_arr[0]) - 1)
     for count in range(len(time_arr)):
         while len(time_arr[count]) < 2:
             time_arr[count] = "0" + time_arr[count]
     dt_string = ""
     try:
-        dt = datetime.datetime(year=date_arr[2], month=date_arr[0], day=date_arr[1], hour=time_arr[0], minute=time_arr[1], second=00)
-        dt_string = dt.strftime('%Y-%m-%dT%H:%M:%S')
+        dt_string += date_arr[2] + "-" + date_arr[0] + "-" + date_arr[1] + "T"
+        dt_string += time_arr[0] + ":" + time_arr[1] + ":00Z"
     except:
         logger.debug(date_arr)
         logger.debug(time_arr)
     return dt_string
 
 if __name__ == '__main__':
-    csv = "ok\n01/14/2019,01/14/2019,LC Logic & Computation(30180)/Lecture,12:00,13:00,Gisbert Kapp LT2 (E202),With:  . Activity: LC Logic & Computation(30180)/Lecture. Type: Lecture. Department: Computer Science"
+    csv = "ok\n01/14/2019,01/14/2019,LC Logic & Computation(30180)/Lecture,12:00,13:00,Gisbert Kapp LT2 (E202),With:  . Activity: LC Logic & Computation(30180)/Lecture. Type: Lecture. Department: Computer Science"
     create_calendar("thm2000","thomas@tmoses.co.uk",csv,2)
