1cada777eaef8a15a2c389005f080d0cf9f46bad
Tom Moses <tom@tmoses.co.uk>
2020-01-15 00:13:06 +0000

    removed /test/progress
    also some other random stuff added as I just hosted some other stuff from this flask app.

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for, Response, jsonify
+from twilio.twiml.messaging_response import MessagingResponse
 import BhamCalConverter
 import time
+import requests
+import pickle
+
 #from flask_recaptcha import ReCaptcha
 
 #site_key = BhamCalConverter.getCAPTCHASiteKey()
@@ -30,6 +34,68 @@ def main():
         time.sleep(5)
         return "posted"
 
+@app.route("/sms2", methods=['GET', 'POST'])
+def incoming_sms2():
+    """Send a dynamic reply to an incoming text message"""
+    # Get the message the user sent our Twilio number
+    body = request.values.get('Body', None)
+
+    # Start our TwiML response
+    resp = MessagingResponse()
+
+    # Determine the right reply for this message
+    if body == 'hello':
+        resp.message("Hi!")
+    elif body == 'bye':
+        resp.message("Goodbye")
+    elif body == None:
+        resp.message("Got none")
+    elif "dog" in body and "cat" in body:
+        resp.message("To be honest and boring, it depends on the sort of relationship you want with your pet. If you want a little buddy that adores you and wants to partake in your life all the time, get a dog. If you want a pet that's more like a roommate providing ambient companionship, seemingly living their own busy life along with being soft and cuddly some of the time, get a cat. And I think cats are hilarious, because they act as if they need to maintain an air of dignity and composure, but constantly fail to do so.")
+
+
+    return str(resp)
+
+@app.route("/sms", methods=['GET', 'POST'])
+def incoming_sms():
+    """Send a dynamic reply to an incoming text message"""
+    # Get the message the user sent our Twilio number
+    body = request.values.get('Body', None)
+
+    # Start our TwiML response
+    resp = MessagingResponse()
+    resp.message("getting your answer...")
+
+    # Determine the right reply for this message
+    if body == None:
+        resp.message("Got none!")
+    elif body.lower == 'bye':
+        resp.message("Goodbye")
+    elif "fab or hackthemidlands" in body.lower():
+        resp.message("Okay, we can figure this out...\n 1. Goto Hack the midlands, \n2. Go to fab\n 3.[removed as under 18's in audience] \n4. go back and re-write your entire hack!")
+    elif "hackthemidlands" in body.lower():
+        resp.message("Goodbye")
+    else:
+        params = {
+            "q" : body,
+            "format" : "json"
+            }
+        r = requests.get("https://api.duckduckgo.com/", params = params)
+        data = r.json()
+        t = data["AbstractText"]
+        if t == "":
+            t = "none found"
+        resp.message(t)
+
+    return str(resp)
+
+
+
+@app.route("/pay", methods=["GET"])
+def payMain():
+    BhamCalConverter.trackVisit()
+    return render_template("new/index-pay.html")
+
 @app.route('/process', methods=['POST'])
 def process():
 	email = request.form['email']
@@ -55,6 +121,10 @@ def queueJump():
 @app.route("/jumper", methods=["GET"])
 def jumper():
     return render_template("new/jumper.html")
+
+@app.route("/reviews")
+def reviews():
+    return render_template("new/reviews.html")
 
 
 @app.route('/_add_numbers')
@@ -140,17 +210,41 @@ def start():
 def extendsTest():
 	return render_template('test_timetable_page.html', stats = BhamCalConverter.getStats())
 
-@app.route('/test/progress')
-def progress():
-	def generate():
-		x = 0
+#@app.route('/test/progress')
+#def progress():
+#	def generate():
+#		x = 0
+#
+#		while x <= 100:
+#			yield "data:" + str(x) + "\n\n"
+#			x = x + 10
+#			time.sleep(2)
+#
+#	return Response(generate(), mimetype= 'text/event-stream')
 
-		while x <= 100:
-			yield "data:" + str(x) + "\n\n"
-			x = x + 10
-			time.sleep(2)
+@app.route("/ubmc/countdown")
+def ubmc_countdown():
+    return render_template("/ubmc/countdown.html", cd_dict = pickle.load( open( "templates/ubmc/cd_dict.p", "rb" ) ))
 
-	return Response(generate(), mimetype= 'text/event-stream')
+@app.route("/ubmc/set/", methods=["GET", "POST"])
+def ubmc_set():
+    if request.method == "GET":
+        return render_template("/ubmc/set.html")
+
+    if request.form["password"] == pickle.load( open( "templates/ubmc/password.p", "rb")):
+        countdown_dict = {"datetime":UBMCDateFormater(request.form["datetime"]),"url":request.form["url"]}
+        pickle.dump( countdown_dict, open( "templates/ubmc/cd_dict.p", "wb" ) )
+        return "worked, set for: " + UBMCDateFormater(request.form["datetime"]) + "was: " + request.form["datetime"]
+    else:
+        return "fail"
+
+    return "other option!"
+
+def UBMCDateFormater(st):
+    st = st.replace("-",",").replace("T",",").replace(":",",").split(",")
+    st = [st[0],str(int(st[1])-1),st[2],st[3],st[4],"05"]
+    st = ", ".join(st)
+    return st
 
 @app.errorhandler(404)
 def page_not_found(e):
